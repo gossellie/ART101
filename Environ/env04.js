dd82c22240b1b14fe3ad1193085da628db2ddbdd
gossellie <gossellie@gmail.com>
2025-11-13 12:33:26 -0800

    debugging

--- Environ/env04.js
+++ Environ/env04.js
@@ -1,39 +1,36 @@
-$(document).ready(function() {
+$(document).ready(function () {
     const Miffy = {
         Breed: "Rabbit",
         age: 4,
         Sex: "female",
         Family: true,
-        Friends: ["Melanie", "Aggie", "Winnie", "Snuffy", "Boris Bear", "Barbara"],
+        Friends: ["Melanie", "Aggie", "Winnie", "Snuffy", "Boris Bear", "Barbara"]
     };
 
-    let megaSentence = "Miffy is a " + Miffy.Breed + " aged " + Miffy.age + " and has friends: " + Miffy.Friends.join(", ");
+    const megaSentence = `Miffy is a ${Miffy.Breed} aged ${Miffy.age} and has friends: ${Miffy.Friends.join(", ")}.`;
     $("#output").html(megaSentence);
 
-    
-
     let onQward = false;
+
     function simpleToggle(primaryBg, altBg, hideImages) {
         if (!onQward) {
-            // switching to the Qward background
-            $('body').css('background-image', "url('" + altBg + "')");
+            $("body").css("background-image", `url("${altBg}")`);
             if (hideImages) {
-                $('#speech, #ah').hide();
-                $('#sinestro').show();
+                $("#speech, #ah").hide();
+                $("#sinestro").show();
             }
         } else {
-            // switching back to the primary background
-            $('body').css('background-image', "url('" + primaryBg + "')");
+            $("body").css("background-image", `url("${primaryBg}")`);
             if (hideImages) {
-                $('#speech, #ah').show();
-                $('#sinestro').hide();
+                $("#speech, #ah").show();
+                $("#sinestro").hide();
             }
         }
         onQward = !onQward;
     }
 
-
-    $('#toggle-env').click(function() {
-        simpleToggle('OA.jpg', 'qward.jpg', true);
+    $("#toggle-env").click(function () {
+        simpleToggle("OA.jpg", "qward.jpg", true);
     });
 });
+
