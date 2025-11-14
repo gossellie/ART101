084bfdfe57dff464fe21d0b11abf8c310bde3ea3
gossellie <gossellie@gmail.com>
2025-11-13 18:33:05 -0800

    avdjgesfuyfdgus

---
+++ Environ/env08.js
@@ -0,0 +1,34 @@
+$(".nice-block span").click(function () {
+  $(this).parent().toggleClass("more");
+
+  if ($(this).html() == "more") {
+    $(this).html("less");
+  } else {
+    $(this).html("more");
+  }
+});
+
+// hover animation
+$(".nice-block").hover(
+  function () {
+    $(this).animate({
+      width: "300px",
+      opacity: 1
+    }, 400);
+  },
+  function () {
+    $(this).animate({
+      width: "200px",
+      opacity: 1
+    }, 400);
+  }
+);
+
+
+$(".Logo-container").mousemove(function(event) {
+  $("#lantern").css({
+    left: event.pageX - 60,
+    top: event.pageY - 60,
+    position: "absolute"
+  });
+});
--- Exercises/creature.js
+++ Exercises/creature.js
@@ -1,8 +1,31 @@
-$("#add-creature").click(
-    function(){
+$("#add-creature").click(function () {
 
     let crName = $("#crName").val();
-    $("#creature-list").append(crName + ", ");
+    let crColor = $("#crColor").val();
+    let crEyesNum = $("#crEyesNum").val();
+    let crWings = $("#crWings").is(":checked");  // NEW
 
-     $("#crName")
+    let crEyesHTML = "";
+    for (let i = 0; i < crEyesNum; i++) {
+        crEyesHTML += "<div class='eye'></div>";
+    }
+
+
+
+    if (crName === "" || crName.length > 12) {
+        return; // invalid name
+    }
+
+    $("#creature-list").append(`
+        <div class="creature">
+            <div class="creature-body" style="background: ${crColor}">
+                ${wingsHTML}
+                ${crEyesHTML}
+                <div class="mouth"></div>
+            </div>
+            <div class="creature-info">${crName}</div>
+        </div>
+    `);
+
+    $("#crName").val("");
 });
